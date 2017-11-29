13dff3ca49271369954763c813013bf14708d177
David Lindahl <davidalindahl@gmail.com>
2017-11-29 14:43:36 -0800

    added variables for filepaths

--- lab-david/index.js
+++ lab-david/index.js
@@ -5,18 +5,20 @@ import { log } from 'util';
 const fs = require('fs');
 
 let flyingCircusPath = `${__dirname}/data/flying-circus.txt`;
+let holyGrailPath = `${__dirname}/../assets/holy-grail.txt`;
+let lifeOfBrianPath = `${__dirname}/../assets/life-of-brian.txt`;  
 
 fs.flyingCircusPath(flyingCircusPath, (error,data) => {
   if(error)
     throw(error);
   console.log('======== break between ipsums ==========');
-  console.log(data.toString('utf-8',0,20));
+  console.log(data.toString('utf-8',0,50));
 
   let readFile(filePath,(error,data) => {
     if(error)
       throw error;
 
     console.log('======== break between ipsums ==========');
-    console.log(data.toString('utf-8',0,20));    
+    console.log(data.toString('utf-8',0,50));    
   });
 });
