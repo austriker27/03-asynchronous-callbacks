103d45d180e4b1ed43302ef35e36205aca46489c
David Lindahl <davidalindahl@gmail.com>
2017-11-29 15:10:18 -0800

    added nested callbacks inside reader file and access files via array

--- lab-david/lib/reader.js
+++ lab-david/lib/reader.js
@@ -3,14 +3,27 @@
 const reader = module.exports = {};
 const fs = require('fs');
 
+let filePathsArray = [`${__dirname}/../assets/flying-circus.txt`, `${__dirname}/../assets/holy-grail.txt`, `${__dirname}/../assets/life-of-brian.txt`];
+
+// let flyingCircusPath = `${__dirname}/../assets/flying-circus.txt`;
+// let holyGrailPath = `${__dirname}/../assets/holy-grail.txt`;
+// let lifeOfBrianPath = `${__dirname}/../assets/life-of-brian.txt`;  
+// let filePathsArray2 = [flyingCircusPath, holyGrailPath, lifeOfBrianPath];
+
 reader.readFile = (callback) => {
-  let flyingCircusPath = `${__dirname}/../assets/flying-circus.txt`;
-  let holyGrailPath = `${__dirname}/../assets/grail-path.txt`;
-  let lifeOfBrianPath = `${__dirname}/../assets/life-of-brian.txt`;  
-  fs.readFile(flyingCircusPath, (error,data) => {
+  fs.readFile(filePathsArray[0], (error,data) => {
     if(error)
       callback(error);
-
-    callback(null,data.toString('utf-8',0,50));  
+    fs.readFile(filePathsArray[1], (error,data) => {
+      if(error)
+        callback(error);
+      fs.readFile(filePathsArray[2], (error,data) => {
+        if(error)
+          callback(error);  
+        callback(null,data.toString('utf-8',0,50));
+      });  
+      callback(null,data.toString('utf-8',0,50));
+    });  
+    callback(null,data.toString('utf-8',0,50));
   });
 };
